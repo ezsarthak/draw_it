dab546c9b6d26f722cbbae75d2f155a4a7c9a9ad
Sarthak Patil <sarthak05patil@gmail.com>
2025-05-28 12:20:25 +0530

    Refactor: Configure Android app for release
    This commit updates the Android application configuration for release.
    
    Key changes include:
    - Updated `applicationId` to `dev.sarthak.drawit`.
    - Set `minSdk` to 21 and `targetSdk` to 35.
    - Added a release signing configuration in `build.gradle.kts` using `keystoreProperties`.
    - Updated the application label in `AndroidManifest.xml` to "Draw it".
    - Added `rename` as a dev dependency in `pubspec.yaml`.
    - Updated app launcher icons (`ic_launcher.png`) across different densities.
    - Added a new asset: `assets/playstore.png`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,20 +21,29 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.draw_new"
+        applicationId = "dev.sarthak.drawit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    signingConfigs {
+        release {
+            keyAlias = keystoreProperties['keyAlias']
+            keyPassword = keystoreProperties['keyPassword']
+            storeFile = keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
+            storePassword = keystoreProperties['storePassword']
+        }
+    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Signing with the debug keys for now,
+            // so `flutter run --release` works.
+            signingConfig = signingConfigs.debug
+            signingConfig = signingConfigs.release
         }
     }
 }
